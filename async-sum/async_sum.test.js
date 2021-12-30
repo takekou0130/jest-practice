86bb98140be8020085b759502c006dfe0ceaa4b2
takekou0130 <60650456+takekou0130@users.noreply.github.com>
2021-12-30 19:55:49 +0900

    add error case

--- async-sum/async_sum.test.js
+++ async-sum/async_sum.test.js
@@ -4,3 +4,10 @@ test("1+2は3になる", async () => {
   const result = await asyncSum(1, 2, 1000);
   expect(result).toBe(3);
 });
+test("int以外は例外をthrowする", async () => {
+  try {
+    await asyncSum("hoge", 2, 1000);
+  } catch (error) {
+    expect(error.message).toMatch("arguments are not number");
+  }
+});
