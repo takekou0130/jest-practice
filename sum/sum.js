b1e7c3080313992cba9a26a9179428b6c4ac1911
takekou0130 <60650456+takekou0130@users.noreply.github.com>
2021-12-30 19:23:17 +0900

    add error test

--- sum/sum.js
+++ sum/sum.js
@@ -1,4 +1,11 @@
 function sum(a, b) {
+  if (typeof a !== "number") {
+    throw new Error("arguments are not number");
+  }
+  if (typeof b !== "number") {
+    throw new Error("arguments are not number");
+  }
   return a + b;
 }
+
 module.exports = sum;
--- sum/sum.test.js
+++ sum/sum.test.js
@@ -3,3 +3,7 @@ const sum = require("./sum");
 test("1+2は3になる", () => {
   expect(sum(1, 2)).toBe(3);
 });
+test("int以外は例外をthrowする", () => {
+  expect(() => sum("hoge", 2)).toThrow();
+  expect(() => sum(1, true)).toThrow();
+});
